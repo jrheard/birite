765b91b7e84d1d6854a238735551b5a925cd4584
jrheard <jrheard@yelp.com>
2012-05-17 14:12:30 -0700

    implement birite

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import datetime
+import urllib2
+
 from flask import Flask
+from pyquery import PyQuery as pq
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def soft_serve():
-    return 'Hello World!'
+	html = pq(urllib2.urlopen('http://www.biritecreamery.com/icrecream').read())
+	days = html("#primary .column-right ul")[3].findall("li")
+	flavors = days[datetime.date.today().weekday()].text_coontent
+	return flavors.split(': ')[1]
+
 
 if __name__ == '__main__':
 	app.run()
